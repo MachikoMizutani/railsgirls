47e91284e6b521ee109a58d91e9f366a0935c3e7
machikomaki <m.machiko@outlook.jp>
2019-08-03 15:40:46 +0900

    coment form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_014832) do
+ActiveRecord::Schema.define(version: 2019_08_03_062821) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
